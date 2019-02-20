1d8279291054c9de3fd0fcb2639256806c0cac49
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-02-20 09:30:13 -0600

    fix merge errors

---
+++ tests/club_test.py
@@ -0,0 +1,74 @@
+import unittest
+from fifa_py.club import *
+from time import sleep
+
+# cs = ClubSummary(club['CLUB_ID'])
+
+class ClubTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+class ClubSummaryTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+class ClubDetailsTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+class ClubDashboardTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+
+class ClubCommonRosterTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+class ClubTeamPlayers(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/match_test.py
@@ -0,0 +1,38 @@
+import unittest
+from fifa_py.match import *
+from time import sleep
+
+class MatchTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+class MatchListTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+class MatchSummaryTest(unittest.TestCase):
+    def setup(self):
+        pass
+    
+    def teardown(self):
+        pass
+    
+    def testA(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
