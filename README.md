9b7fc8ec579bed3f64b35cbd0b503b3f9518b973
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-02-20 09:48:29 -0600

    ci

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Travis](https://img.shields.io/travis/seemethere/nba_py.svg?style=flat-square)](https://travis-ci.org/seemethere/nba_py)   
 
-# futbol_py
+# fifa_py
 A Python API for soccer statistics at fbref.com
 
 ## Installation
--- fifa_py/scoreboard.py
+++ fifa_py/scoreboard.py
@@ -6,4 +6,20 @@ from fifa_py.constants import League
 TODAY = datetime.today()
 
 class Scoreboard:
-    pass
+    '''
+    Displays a scoreboard for all games for a given date
+    '''
+    def __init__(self, date=TODAY, **kwargs):
+        pass
+
+
+    def available(self):
+        '''
+        Gives the available statistics for that day
+        '''
+        pass
+
+    def game_header(self, league=None, **kwargs):
+        if league == None:
+            # return data for all leagues
+            pass
