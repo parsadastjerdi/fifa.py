b08fee4330667622c3426d34d6f60e6dc736826a
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-03-05 19:38:57 -0600

    move playerstats

--- examples/player_example.py
+++ examples/player_example.py
@@ -3,12 +3,12 @@ sys.path.append('../')
 from fifa_py.player import get_pid, Player
 pid = get_pid(first_name='Cristiano', last_name='Ronaldo')
 player = Player(pid)
-player_stats = player.info()
-
 
 for stat in player.info():
     print(stat, player.info()[stat])
 
+
+player_stats = player.info()
 
 player_stats['Apps']
 player_stats['Min']
