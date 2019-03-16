242b03068258347d251044420a639363daced5ad
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-03-15 20:13:31 -0500

    get json method working

--- fifa_py/__init__.py
+++ fifa_py/__init__.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 
-from requests import get, head
+from requests import get, codes
 from requests.exceptions import RequestException
-import json
 
-from bs4 import BeautifulSoup
 
 HAS_PANDAS = True
 try:
@@ -14,44 +12,90 @@ except ImportError:
 
 
 TODAY = datetime.today()
-BASE_URL = 'https://foxsports.com/soccer/{endpoint}'
+BASE_URL = 'https://api.football-data.org/v2/{endpoint}'
 
+key = open('../.api-key').read().strip()
 HEADERS = {
-    'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),  # noqa: E501
-    'Dnt': ('1'),
-    'Accept-Encoding': ('gzip, deflate, sdch'),
-    'Accept-Language': ('en'),
-    'origin': ('https://foxsports.com/soccer/')
+    'X-Auth-Token': key
 }
 
 
-# need to find the correct referer
-def _get_json(endpoint, params, attrs, referer='stats', **kwargs):
+class APIErrorException(Exception):
+    pass
+
+
+def _api_scrape(json, index):
     '''
-    Gets the json object 
-    Input:
-        endpoint:
-        params:
-        referer:
-    Output:
+    Parses the json retrieved from 
+    
+    Args:
+    Returns:
+    Raises:
+    '''
+    pass
+
+
+def _get_json(endpoint, params, **kwargs):
+    '''
+    Streamlines getting json
+
+    Args:
+        endpoint (str): endpoint to be called from the api
+        params (dict): parameters to be passed to the api
+
+    Returns:
         json (json): json object for the selected api call
+
+    Raises:
+        HTTPError: if request status code != 200
+        Exception: if the request code is malformed or bad
+
+    Notes:
+        Is h['referer'] necessary and should the old header be included as well?
     '''
-
     h = dict(HEADERS)
-    h['referer'] = 'https://foxsports.com/{ref}/'.format(ref=referer)
     r = get(BASE_URL.format(endpoint=endpoint), params=params, headers=h)
-    r.raise_for_status() 
+    r.raise_for_status()
+    
+    # exception handling: https://github.com/architv/soccer-cli/blob/master/soccer/request_handler.py
+    if r.status_code == codes.ok:
+        return r.json()
 
-    # convert to BS object b/c can't get it to parse properly
-    soup = BeautifulSoup(r.text, 'html.parser')
-    return soup.find_all('div', attrs=attrs)
+    elif r.status_code == codes.bad:
+        raise APIErrorException('Invalid request. Check parameters.')
+
+    elif r.status_code == codes.forbidden:
+        raise APIErrorException('This resource is restricted')
+
+    elif r.status_code == codes.not_found:
+        raise APIErrorException('This resource does not exist. Check parameters')
+
+    elif r.status_code == codes.too_many_requests:
+        raise APIErrorException('You have exceeded your allowed requests per minute/day')
+
+
+
+def debug(response):
+    '''
+    Prints response header information
+
+    Args:
+
+    Returns:
+        None
+    Raises:
+        None
+    '''
+    print('Response Headers:')
+    print('API Version:', response.headers['X-API-Version'])
+    print('Client:', response.headers['X-Authenticated-Client'])
+    print('Seconds to reset counter:', response.headers['X-RequestCounter-Reset'])
+    print('Requests Available:', response.headers['X-Requests-Available-Minute'])
 
 
 if __name__ == '__main__':
-    endpoint = 'stats'
-    params = {'competition': '1','season': '2018', 'category': 'STANDARD'}
-
-    son = _get_json(endpoint=endpoint, params=params, attrs={'class': 'wisbb_playerContainer'})
-    print(son[1])
-
-  
+    try:
+        json = _get_json(endpoint='competitions/PL/matches', params={'matchday':10})
+        print(json)
+    except Exception as e:
+        print(e)
