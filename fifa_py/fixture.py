c911fd400cfee40b81d4710346e6b3a6ea9c9f66
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-03-23 01:03:55 -0500

    update fixture

---
+++ fifa_py/fixture.py
@@ -0,0 +1,159 @@
+from fifa_py import _api_scrape, _get_json, _form_endpoint
+from fifa_py.constants import CURRENT_SEASON
+
+class Fixture:
+    '''
+    Returns data related to a single fixture
+    How is this different from FixtureSummary
+    Args:
+    Returns:
+    Raises:
+    Notes:
+    '''
+
+    _endpoint = 'fixtures'
+    
+    def __init__(self, 
+                fixture_id, 
+                api_key,
+                include=None,
+                **kwargs):
+        endpoint = _form_endpoint([self._endpoint, fixture_id])
+        self._api_key = api_key
+        self._include = include
+        self.json = _get_json(endpoint=endpoint, 
+                                api_key=api_key,
+                                include={'include': include})
+    
+    def info(self):
+        return _api_scrape(json=self.json, 
+                            key=['data'], 
+                            exclude=None)
+
+    def details(self):
+        return _api_scrape(json=self.json,
+                            key=['data'],
+                            exclude=['weather_report', 'formations', 'scores', 'time', 'coaches', 'standings', 'assistants', 'colors'])
+    
+    def meta(self):
+        return _api_scrape(json=self.json,
+                            key=['meta'],
+                            exclude=None)
+    
+    def weather_report(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'weather_report'],
+                            exclude=None)
+    
+    def scores(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'scores'],
+                            exclude=None)
+    
+    def time(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'time'],
+                            exclude=None)
+
+    def coaches(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'coaches'],
+                            exclude=None)
+    
+    def standings(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'standings'],
+                            exclude=None)
+
+    def colors(self):
+        return _api_scrape(json=self.json,
+                            key=['data', 'colors'],
+                            exclude=None)
+
+    def include(self):
+        try:
+            return _api_scrape(json=self.json,
+                                key=['data', self._include],
+                                exclude=None)
+        except Exception as e:
+            print(e)
+            return None
+
+
+
+class FixtureList:
+    '''
+    Returns a list of matches, can be more specific given a league or time frame
+    
+    Args:
+    Returns:
+    Raises:
+    '''
+
+    _endpoint = 'seasons'
+
+    def __init__(self, 
+                matchday=None,
+                status=None,
+                league_id=None,
+                dateTo=None,
+                dateFrom=None,
+                **kwargs):
+        endpoint = _form_endpoint([self._endpoint])
+        self.json = _get_json(endpoint=endpoint, 
+                                include={'include': 'fixtures'})
+        
+
+    def info(self):
+        return _api_scrape(json=self.json, 
+                            key=['fixtures'],
+                            exclude=None)
+
+
+
+
+class Head2Head:
+    '''
+    Returns data related to a single match
+    Args:
+    Returns:
+    Raises:
+    '''
+    _endpoint = 'matches'
+    
+    def __init__(self, match_id, **kwargs):
+        endpoint = _form_endpoint([self._endpoint, match_id])
+        self.json = _get_json(endpoint=endpoint)
+    
+    def info(self):
+        return _api_scrape(json=self.json, 
+                            key=['head2head'], 
+                            exclude=['homeTeam', 'awayTeam'])
+
+    def hometeam(self):
+        return _api_scrape(self.json,
+                            key=['homeTeam'],
+                            exclude=None)
+    
+    def awayteam(self):
+        return _api_scrape(self.json,
+                            key=['awayTeam'],
+                            exclude=None)
+
+
+# ------------------------------------------------------- #
+#               Extra classes                             #
+# ------------------------------------------------------- #
+
+
+class MatchLineup:
+    '''
+    Returns the lineup for a specific match
+
+    Args:
+    Returns:
+    Raises:
+    '''
+
+    def __init__(self, match_id, **kwargs):
+        pass                           
